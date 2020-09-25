a8a563ad0157b939d1cb8354d0e5c245a9b9d275
JNchengge <chenghao5678@stu2019.jnu.edu.cn>
2020-09-25 16:30:03 +0800

    update

--- python/test.py
+++ python/test.py
@@ -1,15 +1,4 @@
-def next_state_list(state,a,b,c,l):
-    if l==1:
-        state=state[::-1]
-        state[state.index(a)]=c
-        state=state[::-1]
-        print(state)
-        return state
-    else:
-        state=next_state_list(state,a,c,b,l-1)
-        state=next_state_list(state,a,b,c,1)
-        state=next_state_list(state,b,a,c,l-1)
-        return state
-
-a,b,c='1','2','3'
-next_state_list(['1','1','1'],a,b,c,3)
+a=['d','y','v']
+b=sorted(a,key=str.upper,reverse=True)
+print(b)
+print(a is b)

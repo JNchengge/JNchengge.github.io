1298c8fd9d75ce530ea99571fe9a0208aa428f0e
JNchengge <chenghao5678@stu2019.jnu.edu.cn>
2020-10-30 21:11:23 +0800

    update

--- ctf/code/test.py
+++ ctf/code/test.py
@@ -1,31 +1,9 @@
-f1=open('msg001.enc','r')
-f2=open('msg002.enc','r')
-cipher1=f1.read()
-cipher2=f2.read()
-n=0
-t=0
-input_="Hi! This is only test message"
-key='VeryLongKeyYouWillNeverGuess'*1000
-for j,p in zip(cipher1,input_): #求解密钥
-    for k in range(256):
-        c=(ord(p)+(k^t)+n*n)&0xff
-        if chr(c)==j:
-            t=ord(p)
-            n+=1
-            key+=chr(k)
-            break
-print(key)
-n=0
-t=0
-flag=''
-for i in cipher2:  #解密
-    for p in range(256):
-        c=(p+(ord(key[n])^t)+n*n)&0xff
-        if i==chr(c):
-            t=p
-            n+=1
-            flag+=chr(p%128)
-            break
-print(flag)
-f1.close()
-f2.close()
+from libnum import n2s,s2n
+ctxt1=0xaf3fcc28377e7e983355096fd4f635856df82bbab61d2c50892d9ee5d913a07f
+ctxt2=0x630eb4dce274d29a16f86940f2f35253477665949170ed9e8c9e828794b5543c
+ctxt3=0xe913db07cbe4f433c7cdeaac549757d23651ebdccf69d7fbdfd5dc2829334d1b
+fake_secret1 = "I_am_not_a_secret_so_you_know_me"
+fake_secret2 = "feeddeadbeefcafefeeddeadbeefcafe"
+P = 0x10000000000000000000000000000000000000000000000000000000000000425
+key2=ctxt2^s2n(fake_secret1)
+key3=ctxt3^s2n(fake_secret2)

1a9d1b512dac659b7fae758f14ff02cc791e933b
JNchengge <chenghao5678@stu2019.jnu.edu.cn>
2020-11-11 20:24:34 +0800

    update

--- ctf/test_dir/test.py
+++ ctf/test_dir/test.py
@@ -1,11 +1,3 @@
-s='c61b68366edeb7bdce3c6820314b7498'
-t=['0' for i in range(32)]
-v=0
-while(v<len(s)):
-    if v&1:
-        v3=1
-    else:
-        v3=-1
-    t[v]=chr(ord(s[v])+v3)
-    v+=1
-print(''.join(t))
+from libnum import n2s
+a=0x437261636b4d654a757374466f7246756e
+print(n2s(a))
